bae818102f939f413bd928e80b19a73084ac713a
elkabrito <alberto8abernal@gmail.com>
2022-06-02 10:59:44 -0500

    app calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import freeCodeCampLogo from "./images/freecodecamp-logo.png";
+
+import Boton from "./components/Boton.js";
+import Pantalla from "./components/Pantalla.js";
+import BotonClear from "./components/BotonClear.js";
+
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const addInput = (val) => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    setInput(evaluate(input));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="Logo de freeCodeCamp"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+        <Pantalla input={input} />
+        <div className="fila">
+          <Boton manejarClic={addInput}>1</Boton>
+          <Boton manejarClic={addInput}>2</Boton>
+          <Boton manejarClic={addInput}>3</Boton>
+          <Boton manejarClic={addInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={addInput}>4</Boton>
+          <Boton manejarClic={addInput}>5</Boton>
+          <Boton manejarClic={addInput}>6</Boton>
+          <Boton manejarClic={addInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={addInput}>7</Boton>
+          <Boton manejarClic={addInput}>8</Boton>
+          <Boton manejarClic={addInput}>9</Boton>
+          <Boton manejarClic={addInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={addInput}>0</Boton>
+          <Boton manejarClic={addInput}>.</Boton>
+          <Boton manejarClic={addInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear manejarClear={() => setInput("")}>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Pantalla.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "../css/Pantalla.css";
+
+const Pantalla = ({ input }) => <div className="input">{input}</div>;
+
+export default Pantalla;
